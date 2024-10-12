8b23f2bc6264d80216fb04e5d15d512164f673ef
H82M19\Student <Student>
2024-10-12 18:36:35 +0700

    Init Tuan05

---
+++ LeDonChung_Lab_Week05/src/main/java/vn/edu/iuh/fit/donchung/LeDonChungLabWeek05Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.donchung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeDonChungLabWeek05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeDonChungLabWeek05Application.class, args);
+	}
+
+}
