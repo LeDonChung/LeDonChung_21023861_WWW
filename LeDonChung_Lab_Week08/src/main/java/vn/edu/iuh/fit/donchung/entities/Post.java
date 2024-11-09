400b0e02f7b2c93ca7066ac40fe0bb2ee746919c
H82M19\Student <Student>
2024-11-09 19:09:36 +0700

    Create Project Lab Week 8

---
+++ LeDonChung_Lab_Week08/src/main/java/vn/edu/iuh/fit/donchung/entities/Post.java
@@ -0,0 +1,64 @@
+package vn.edu.iuh.fit.donchung.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "authorId", nullable = false)
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parentId")
+    private Post parent;
+
+    @Column(name = "title", nullable = false, length = 75)
+    private String title;
+
+    @Column(name = "metaTitle", length = 100)
+    private String metaTitle;
+
+    @Lob
+    @Column(name = "summary")
+    private String summary;
+
+    @Column(name = "published", nullable = false)
+    private Boolean published = false;
+
+    @Column(name = "createdAt", nullable = false)
+    private Timestamp createdAt;
+
+    @Column(name = "updatedAt")
+    private Timestamp updatedAt;
+
+    @Column(name = "publishedAt")
+    private Timestamp publishedAt;
+
+    @Lob
+    @Column(name = "content")
+    private String content;
+
+    @OneToMany(mappedBy = "parent")
+    private Set<Post> posts = new LinkedHashSet<>();
+
+    @OneToMany(mappedBy = "post")
+    private Set<PostComment> postComments = new LinkedHashSet<>();
+
+}
---
+++ LeDonChung_Lab_Week08/src/main/java/vn/edu/iuh/fit/donchung/entities/User.java
@@ -0,0 +1,60 @@
+package vn.edu.iuh.fit.donchung.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "firstName", length = 50)
+    private String firstName;
+
+    @Column(name = "middleName", length = 50)
+    private String middleName;
+
+    @Column(name = "lastName", length = 50)
+    private String lastName;
+
+    @Column(name = "mobile", length = 15)
+    private String mobile;
+
+    @Column(name = "email", length = 50)
+    private String email;
+
+    @Column(name = "passwordHash", nullable = false, length = 32)
+    private String passwordHash;
+
+    @Column(name = "registeredAt", nullable = false)
+    private Timestamp registeredAt;
+
+    @Column(name = "lastLogin")
+    private Timestamp lastLogin;
+
+    @Lob
+    @Column(name = "intro")
+    private String intro;
+
+    @Lob
+    @Column(name = "profile")
+    private String profile;
+
+    @OneToMany(mappedBy = "author")
+    private Set<Post> posts = new LinkedHashSet<>();
+
+}
