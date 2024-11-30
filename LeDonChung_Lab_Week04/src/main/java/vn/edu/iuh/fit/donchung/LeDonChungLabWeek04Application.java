a19561e57ef89699fc1c138183e2f10bbcb3bf45
LeDonChung <ledonchung12a2@gmail.com>
2024-11-30 17:11:30 +0700

    Change Class Application Week04

---
+++ LeDonChung_Lab_Week04/src/main/java/vn/edu/iuh/fit/donchung/LeDonChungLabWeek04Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.donchung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeDonChungLabWeek04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeDonChungLabWeek04Application.class, args);
+    }
+
+}
